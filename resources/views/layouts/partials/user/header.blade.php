fb40be629135f4286075051202d9d5e19d9be87d
bimaega15 <bimaega15@gmail.com>
2023-08-08 19:25:24 +0700

    sad boy

--- resources/views/layouts/partials/user/header.blade.php
+++ resources/views/layouts/partials/user/header.blade.php
@@ -11,146 +11,27 @@
             </button>
             <div class="collapse navbar-collapse" id="mainNavigation">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav__item  has-dropdown">
-                        <a href="index.html" data-toggle="dropdown" class="dropdown-toggle nav__item-link active">Home</a>
-                        <ul class="dropdown-menu">
-                            <li class="nav__item">
-                                <a href="index.html" class="nav__item-link">Home Main</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="home-modern.html" class="nav__item-link">Home Modern</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="home-classic.html" class="nav__item-link">Home Classic</a>
-                            </li><!-- /.nav-item -->
-                        </ul><!-- /.dropdown-menu -->
-                    </li><!-- /.nav-item -->
-                    <li class="nav__item  has-dropdown">
-                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Company</a>
-                        <ul class="dropdown-menu">
-                            <li class="nav__item">
-                                <a href="about-us.html" class="nav__item-link">About Us</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="why-us.html" class="nav__item-link">Why Choose Us</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="leadership-team.html" class="nav__item-link">Leadership Team</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="awards.html" class="nav__item-link">Award & Recognition</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="pricing.html" class="nav__item-link">Pricing & Plans</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="faqs.html" class="nav__item-link">Help & FAQs</a>
-                            </li> <!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="careers.html" class="nav__item-link">Careers</a>
-                            </li><!-- /.nav-item -->
-                        </ul><!-- /.dropdown-menu -->
-                    </li><!-- /.nav-item -->
-                    <li class="nav__item  has-dropdown">
-                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">IT Solutions</a>
-                        <ul class="dropdown-menu wide-dropdown-menu">
-                            <li class="nav__item">
-                                <div class="row mx-0">
-                                    <div class="col-sm-6 dropdown-menu-col">
-                                        <a href="it-solutions.html" class="nav__item-link dropdown-menu-title">IT Solutions</a>
-                                        <ul class="nav flex-column">
-                                            <li class="nav__item"><a class="nav__item-link" href="it-solutions-single.html">IT
-                                                    Management</a>
-                                            </li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="it-solutions-single.html">Cyber
-                                                    Security</a>
-                                            </li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="it-solutions-single.html">Cloud
-                                                    Computing</a>
-                                            </li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="it-solutions-single.html">IT
-                                                    Consulting</a>
-                                            </li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="it-solutions-single.html">Software
-                                                    Dev</a>
-                                            </li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="it-solutions-single.html">IT Support</a>
-                                            </li>
-                                            <!-- /.nav-item -->
-                                        </ul>
-                                    </div><!-- /.col-sm-6 -->
-                                    <div class="col-sm-6 dropdown-menu-col">
-                                        <a href="industries.html" class="nav__item-link dropdown-menu-title">Industries</a>
-                                        <ul class="nav flex-column">
-                                            <li class="nav__item"><a class="nav__item-link" href="industries-single-industry.html">Education,
-                                                    Non-Profit</a></li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="industries-single-industry.html">Accounting,
-                                                    Finance</a></li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="industries-single-industry.html">Government &
-                                                    Public</a></li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="industries-single-industry.html">Energy
-                                                    &
-                                                    Utilities</a></li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="industries-single-industry.html">Legal,
-                                                    Law
-                                                    Firms</a></li> <!-- /.nav-item -->
-                                            <li class="nav__item"><a class="nav__item-link" href="industries-single-industry.html">Manufacturing</a>
-                                            </li>
-                                            <!-- /.nav-item -->
-                                        </ul>
-                                    </div><!-- /.col-sm-6 -->
-                                </div><!-- /.row -->
-                            </li><!-- /.nav-item -->
-                        </ul><!-- /.dropdown-menu -->
-                    </li><!-- /.nav-item -->
-                    <li class="nav__item  has-dropdown">
-                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">News&Media</a>
-                        <ul class="dropdown-menu">
-                            <li class="nav__item">
-                                <a href="blog.html" class="nav__item-link">Our Blog</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="blog-single-post.html" class="nav__item-link">Single Blog Post</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="case-studies-grid.html" class="nav__item-link">Case Studies Grid</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="case-studies-carousel.html" class="nav__item-link">Case Studies Carousel</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="case-studies-classic.html" class="nav__item-link">Case Studies Classic</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="case-studies-single.html" class="nav__item-link">Single Case Study</a>
-                            </li><!-- /.nav-item -->
-                        </ul><!-- /.dropdown-menu -->
-                    </li><!-- /.nav-item -->
-                    <li class="nav__item  has-dropdown">
-                        <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Features</a>
-                        <ul class="dropdown-menu">
-                            <li class="nav__item">
-                                <a href="coming-soon.html" class="nav__item-link">Coming Soon</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="404.html" class="nav__item-link">404 Page</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="#" class="nav__item-link  open-register-popup">Register</a>
-                            </li><!-- /.nav-item -->
-                            <li class="nav__item">
-                                <a href="#" class="nav__item-link  open-login-popup">Login</a>
-                            </li><!-- /.nav-item -->
-                        </ul><!-- /.dropdown-menu -->
+                    <li class="nav__item">
+                        <a href="contact-us.html" class="nav__item-link active">Home</a>
                     </li><!-- /.nav-item -->
                     <li class="nav__item">
-                        <a href="contact-us.html" class="nav__item-link">Contacts</a>
+                        <a href="contact-us.html" class="nav__item-link">About</a>
                     </li><!-- /.nav-item -->
+                    <li class="nav__item">
+                        <a href="contact-us.html" class="nav__item-link">Gallery</a>
+                    </li><!-- /.nav-item -->
+                    <li class="nav__item">
+                        <a href="contact-us.html" class="nav__item-link">Contact Us</a>
+                    </li><!-- /.nav-item -->
+                    <li class="nav__item">
+                        <a href="contact-us.html" class="nav__item-link">TPS</a>
+                    </li><!-- /.nav-item -->
+
                 </ul><!-- /.navbar-nav -->
             </div><!-- /.navbar-collapse -->
             <ul class="navbar-actions list-unstyled mb-0 d-flex align-items-center">
                 <li class="d-none d-xl-block">
-                    <a href="request-quote.html" class="btn action__btn-contact">Request A Quote</a>
+                    <a href="request-quote.html" class="btn action__btn-contact"><i class="fas fa-user-tag"></i> Pendaftaran C.O</a>
                 </li>
                 <li>
                     <button class="action__btn action__btn-login open-login-popup">
